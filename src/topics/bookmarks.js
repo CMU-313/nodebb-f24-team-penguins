618515937e3ccc121c212d05bfbe25568c5a0b60
Andrew <andrewwang12345@gmail.com>
2024-10-10 22:42:12 -0400

    add post and update for bookmark

--- src/topics/bookmarks.js
+++ src/topics/bookmarks.js
@@ -7,6 +7,48 @@ const db = require('../database');
 const user = require('../user');
 
 module.exports = function (Topics) {
+	Topics.setBookmark = async function (tid, uid, index) {
+		if (parseInt(uid, 10) <= 0) {
+			return;
+		}
+		await db.sortedSetAdd(`uid:${uid}:bookmarks`, index, tid);
+	};
+
+	Topics.updateBookmark = async function (tid, pids) {
+		const maxIndex = await Topics.getPostCount(tid);
+		const indices = await db.sortedSetRanks(`tid:${tid}:posts`, pids);
+		const postIndices = indices.map(i => (i === null ? 0 : i + 1));
+		const minIndex = Math.min(...postIndices);
+
+		const bookmarks = await Topics.getBookmarks(tid);
+
+		const uidData = bookmarks.map(b => ({ uid: b.value, bookmark: parseInt(b.score, 10) }))
+			.filter(data => data.bookmark >= minIndex);
+
+		await async.eachLimit(uidData, 50, async (data) => {
+			let bookmark = Math.min(data.bookmark, maxIndex);
+
+			postIndices.forEach((i) => {
+				if (i < data.bookmark) {
+					bookmark -= 1;
+				}
+			});
+
+			// make sure the bookmark is valid if we removed the last post
+			bookmark = Math.min(bookmark, maxIndex - pids.length);
+			if (bookmark === data.bookmark) {
+				return;
+			}
+
+			const settings = await user.getSettings(data.uid);
+			if (settings.postSort === 'most_votes') {
+				return;
+			}
+
+			await Topics.setBookmark(tid, data.uid, bookmark);
+		});
+	};
+
 	Topics.getUserBookmark = async function (tid, uid) {
 		if (parseInt(uid, 10) <= 0) {
 			return null;
